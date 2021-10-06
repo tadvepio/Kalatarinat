542a101513d612c3958794e8c399e83344baa2b1
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-10-06 03:15:32 +0300

    Created FishStore

---
+++ front-end/stores/FishStore.js
@@ -0,0 +1,47 @@
+import {decorate, observable, action} from 'mobx';
+import {create, persist} from 'mobx-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export class FishStore {
+
+    data = [
+        {
+            ID: 0,
+            date: '12.11.2021',
+            time: '14:20',
+            location: 'Turku',
+            temperature: 10,
+            weather: 'cloudy',
+            equipment: [
+                {
+                    ID: 0,
+                    item: 'viehe',
+                    name: null,
+                }
+            ],
+            fish: [
+                {
+                    ID: 0,
+                    species: 'hauki',
+                    weight: 1.3,
+                    length: 40,
+                }
+            ],
+            other_info: 'Syöttinä käytety maan matosia',
+        },
+    ];
+
+    save() {};
+      
+}
+
+decorate(FishStore, {
+    data: [observable, persist('list')],
+
+    save: action,
+});
+  
+const hydrate = create({storage: AsyncStorage});
+const fishStore = new FishStore();
+export default fishStore;
+hydrate('fishnStore', fishStore);
