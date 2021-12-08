c4dfbc5ba90a0f567433a8a383ed3eced7f64ef1
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-12-08 03:22:34 +0200

    Updated the navigation

--- front-end/navigation/AppNavigator.js
+++ front-end/navigation/AppNavigator.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
 import CreateEntryScreen from '../screens/CreateEntryScreen';
 import ModifyEntryScreen from '../screens/ModifyEntryScreen';
+import AllEntriesScreen from '../screens/AllEntriesScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -20,12 +22,12 @@ export default function AppNavigator() {
                 }}
             >
 
-                {/*Home*/}
-                <Stack.Screen name='HomeScreen' options={{headerShown: false}} component={HomeScreen} />
-                  
                 <Stack.Screen name='LoginScreen' options={{headerShown: false}} component={LoginScreen} />
+                <Stack.Screen name='HomeScreen' options={{headerShown: false}} component={HomeScreen} />
                 <Stack.Screen name='CreateEntryScreen' options={{title: 'Uusi merkintä'}} component={CreateEntryScreen} />
                 <Stack.Screen name='ModifyEntryScreen' options={{title: 'Muokkaa merkintää'}} component={ModifyEntryScreen} />
+                <Stack.Screen name='AllEntriesScreen' options={{title: 'Kaikki merkinnät', headerLeft: null}} component={AllEntriesScreen} />
+                <Stack.Screen name='ProfileScreen' options={{title: 'Oma profiili', headerLeft: null}} component={ProfileScreen} />
 
             </Stack.Navigator>
         </NavigationContainer>
